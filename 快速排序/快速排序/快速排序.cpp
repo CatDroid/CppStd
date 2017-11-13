742379cfe8e98d3df21cac3fcbd06d2dbffcd036
hanloong ho <1198432354@qq.com>
2017-11-13 10:45:32 +0800

    快速排序 不适宜有重复数据 , 解决只有两个元素时候的排序

--- 快速排序/快速排序/快速排序.cpp
+++ 快速排序/快速排序/快速排序.cpp
@@ -34,11 +34,11 @@ void quick_sort(DATA* arr, int start, int end) {
 		arr[right] = temp;
 	}
 
-	//DATA temp = arr[left];
-	//arr[left] = arr[start];
-	//arr[start] = temp;
-	arr[start] = arr[left];
-	arr[left] = ref;
+	if (arr[left] < arr[start]) { // 避免只有两个元素时候
+		arr[start] = arr[left];
+		arr[left] = ref;
+	}
+
 
 	// 在 left == right 处 分开了两边 ，左边都小于参考ref  右边都大于参考ref 
 	quick_sort(arr, start , left-1);
@@ -55,14 +55,18 @@ void dump(DATA* dat , int length ) {
 	cout << "dump end   " << endl;
 }
 
+
 // 快速排序的最差时间复杂度和冒泡排序是一样的都是O(N2)，它的平均时间复杂度为O(NlogN)
 
 int main()
 {
 	DATA d[] = { 6,1,2,7,9,3,4,5,10,8 };
+	//DATA d[] = { 6,10,2,7,9,3,4,10,10,8 }; // 注意快速排序不适宜解决有重复数据的排序 
+	//DATA d[] = {1,2  };
 	
 	dump(d, sizeof(d) / sizeof(d[0]));
 	quick_sort(d, 0, sizeof(d)/sizeof(d[0]) - 1 );
+	cout << "---after sort---" << endl;
 	dump(d, sizeof(d) / sizeof(d[0]));
 	
     return 0;
