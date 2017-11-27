5c214b142fa384a46503a0f21444dd831b759953
hohanloong <1198432354@qq.com>
2017-11-27 23:40:11 +0800

    lambda 1. 值传递 只读  引用传递 读写 2. lambda在类中成员函数中定义 那么这个lambda函数是这个类的friend 3. lambda函数 作为参数传入，该类型必须是模版类型，或是必须，创建一个std::function ,去获取lambda的值 4. 全局变量 静态变量 无法在lambda中捕获 5. 闭包: 在 lambda函数 的定义所在函数以外 调用, 并且捕捉 定义所在函数的局部变量 6. lambda中需要访问类的成员，需要捕捉this

--- cpp11_lambda/cpp11_lambda/cpp11_lambda.cpp
+++ cpp11_lambda/cpp11_lambda/cpp11_lambda.cpp
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <algorithm>
 #include <iostream>
+#include <vector>
 
 using namespace std ;
 
@@ -25,9 +26,12 @@ static int intcompare(const T *p1, const T *p2)
 
 class MyClass {
 private:
-	
-public:
 	int more;
+	int privateFun() {
+		return more;
+	}
+public:
+	
 	MyClass(int temp):more(temp) {
 		cout << "MyClass()" << endl;
 	}
@@ -50,6 +54,22 @@ public:
 		int result = b2(200);
 	}
 
+	/*
+		若一个 lambda函数被定义于某类型的成员函数中，会被当作该类型的friend
+		只有当该 lambda函数创建的 作用域 是在 该类的成员函数内部时才能运作
+		在成员函数中指涉对象的this指针，必须要显式的传入 lambda 函数， 否则成员函数中的 lambda 函数无法使用任何该对象的参数或函数
+		若是 lambda 函数使用 [&] 或是 [=] 的形式，this在 lambda 函数即为可见
+
+		使用 auto 关键字让我们能够存储 lambda 函数
+
+
+	*/
+	auto getLambda(int a ){
+		return [this ,a]()->int{  return this->privateFun() * a ; /*friend*/ };
+	}
+
+ 
+
 };
 
 /*
@@ -58,17 +78,26 @@ public:
 	[](int x, int y) -> int { int z = x + y; return z + x; };
 	如果我们将 lambda表达式 比作 函数，可以看到它与“函数名”对应的是一对空的“方括号”，即“捕获表达式”
 	 
+	[](int x, int y) { return x + y; }
+	匿名函数的回返类型是 decltype(x+y)。只有在 lambda 函数符合"return expression"的形式下，它的返回类型才能被忽略
+	如果 lambda 函数没有传回值(例如 void )，其回返类型可被完全忽略
 
-
+	传值/传引用:
 	[var]   传值方式（只读）将变量传递到匿名函数中
 	[=]     父作用范围的所有变量以传值方式（只读）,传递到匿名函数中（包括this）
 	[&var]  引用方式（读写）将变量传递到匿名函数中
 	[&]     父作用范围的所有变量以引用方式（读写）,传递到匿名函数中（包括this）
 	[=,&var1,&var]  var1,var2以读写方式传递到匿名函数中，其他变量以只读方式传递到函数中
 	[&, var1,this]   var1,this以只读方式传递到函数中，其他变量以读写方式传递到函数中
+	定义在 与lambda函数 相同作用域的参数/引用 也可以被使用, 参数集合一般被称作 closure (闭包)
+	若不使用引用的符号&，则代表参数以传值的方式传入 lambda 函数  , "传值的话就是只读"
 
 	捕捉非父作用范围的变量，或者非自变量（如，静态常量）等，都将导致编译错误。故可以看出，匿名函数
-	非动态函数，在编译期就定义了，故不会产生额外开销。
+	非动态函数，在编译期就定义了，故不会产生额外开销
+
+	lambda函数可以不在被声明的地方就地使用,若参数是以传引用的方式链接到 closure中,是无意义甚至是危险的行为
+	若lambda 函数只在定义的作用域使用， 则可以用 [&] 声明 lambda 函数，代表所有引用到栈/stack中的参数，都是以引用的方式传入
+
 */
 int main()
 {
@@ -108,6 +137,58 @@ int main()
 		 
 		int result = b2(200);
 		cout  << "b = " << b << " result = " << result << endl;
+	}
+
+	{ // 多次调用  lambda表达式 (没有闭包 只是一个局部的匿名函数)
+		auto ftn = [](int x, int y) -> int { int z = x + y; return z + x; }; 
+		cout << ftn(3, 2) << endl; // 8
+		cout << ftn(3, 3) << endl; // 9 	
+	}
+
+	{
+		int total = 0;
+		int value = 5;
+		auto ftn = [&, value](int x) { total += (x * value); return total; };
+		cout << ftn(3) << endl; // 15 
+		value = 6;  // 值传递 不影响
+		cout << ftn(4) << endl; // 35 
+
+	}
+	{ // 闭包 与 std::vector  std::for_each 
+		std::vector<int> someList;
+		for (int i = 0; i < 10; i++) {
+			someList.push_back(i);
+		}
+		int total = 0;
+		std::for_each(someList.begin(), someList.end(), [&total](int x) { total += x;});
+		cout << total << endl; // 45
+	}
+
+	{
+		MyClass my(12);
+		auto ftn = my.getLambda(4); // 48
+		cout << ftn() << endl;
+	}
+
+
+
+	{
+		/*
+			lambda函数 是编译器从属类型的函数对象, 这种类型名称只有编译器自己能够使用
+			如果用户希望将 lambda函数 作为参数传入，该类型必须是模版类型，或是必须，创建一个std::function ,去获取lambda的值
+		
+			????
+			但是，如果 lambda 函数是以‘引用’的方式获取到它‘所有’的 closure 参数，或者是没有 closure 参数，
+			那么所产生的函数对象会被给予一个特殊的类型:std::reference_closure<R(P)>，
+			其中 R(P) 是包含回返类型的函数签名。
+			比起由 std::function 获取而来，这会是lambda函数更有效率的代表：
+			std::reference_closure<void()> myLambdaFunc = [this]() { this->SomePrivateMemberFunction(); };
+			myLambdaFunc();
+
+		*/
+		auto myOnheapLambdaFunc = new auto( [=]()->int{ return 2; });
+		cout << (*myOnheapLambdaFunc)() << endl;  // 在堆上分配的lambda表达式
+		delete myOnheapLambdaFunc;
 	}
 
     return 0;
