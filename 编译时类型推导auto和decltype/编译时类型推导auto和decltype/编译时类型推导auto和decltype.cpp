216eddf18fc5ef4f3b379a11741b2e10d1447e9c
hohanloong <1198432354@qq.com>
2017-12-02 11:57:34 +0800

    decltype 类型推倒 1. 推倒成右值引用：本来就是右值引用  或者  将亡值 2. 非单个标记符的表达式 是左值的话，那么推到成左值 3. 重载函数decltype将会错误 无法确定需要哪个重载函数实例 4. 字符串字面值常量是个左值，且是const左值，而非字符串字面值常量则是个右值 5. is_lvalue_reference 和 is_rvalue_reference 是模板类 判断decltype推倒的类型是否左值引用或者右值引用用 6. int i ; decltype(i++) 既不是右值引用 也不是左值引用 i++本身返回右值 但不是将忙值 所以推倒为int类型 7. 将亡值 是 返回右值引用 或者 转换为右值引用

--- 编译时类型推导auto和decltype/编译时类型推导auto和decltype/编译时类型推导auto和decltype.cpp
+++ 编译时类型推导auto和decltype/编译时类型推导auto和decltype/编译时类型推导auto和decltype.cpp
@@ -163,6 +163,85 @@ int main()
 	
 	// 泛型编程中结合auto，用于追踪函数的返回值类型
 	auto ret = multiply(2.0, 3.0);
+
+
+	/*
+		decltype推导四规则:
+
+		如果e是一个‘没有带括号’的‘标记符表达式’或者‘类成员 访问表达式’，decltype（e）就是e所命名的实体的类型。
+		此外，如果e是一个被重载的函数，则会导致编译错误(无法确定需要哪个重载函数实例)
+		否则，假设e的类型是T，如果e是一个将亡值，那么decltype（e）为T&&
+		否则，假设e的类型是T，如果e是一个左值，那么decltype（e）为T&。
+		否则，假设e的类型是T，则decltype（e）为T
+
+		'标记符'指的是除去'关键字、字面量'等编译器需要使用的标记之外的程序员自己定义的标记
+
+		‘单个标记符’对应的表达式即为‘标记符表达式’ int arr[4]  arr为一个标记符表达式(可以decltype(arr))，而arr[3]+0不是
+	
+		字符串字面值常量是个左值，且是const左值，而非字符串字面值常量则是个右值
+
+		“将亡值”概念的产生，是由右值引用的产生而引起的，将亡值与右值引用息息相关。
+		所谓的将亡值表达式，就是下列表达式：
+		1）返回"右值引用的函数"的调用表达式
+		2）"转换为右值引用的转换函数"的调用表达式
+
+		C++11标准库 模板类
+		is_lvalue_reference 来判断 表达式是否为左值		结果1表示为左值，结果为0为非右值
+		is_rvalue_reference 来判断 decltype推断结果是否为右值
+
+	*/
+	{
+		int tempi = 10;
+		decltype(tempi) test = 2;	// 推倒为int 没有括号-->标记符-->单个标记符--> 标记符表达式 
+		decltype(tempi + 1) test2 = 2; // 推倒为int 
+		decltype((tempi + 1)) test3 = 3;// 还是推倒为int 
+		decltype(tempi) a;			// a推导为int：i是一个标记符表达式，根据推导规则1，类型被推导为int
+		decltype((tempi)) b = tempi;// b推倒为int& 必须初始化：(i)为一个左值表达式，类型被推导为int&
+		
+		// 先声明 
+		int i = 4;
+		int arr[5] = { 0 };
+		int *ptr = arr;
+		struct S { double d; }s;
+		void Overloaded(int);  // 函数内部可以声明函数 但是不能定义函数
+		void Overloaded(char); // 重载的函数
+		int && RvalRef();
+		const bool Func(int);
+		Person ppp;
+
+		//规则一：推导为其类型( 都是没有带括号的 )
+		decltype (arr) var1;	//	int		标记符表达式
+		decltype (ptr) var2;	//	int *	标记符表达式
+		decltype (s.d) var3;	//	double	成员访问表达式
+		//decltype(ppp_person_type) var4;   成员访问表达式 私有成员是不可以的
+		//decltype(Overloaded) var4;//重载函数。编译错误:无法确定需要哪个重载函数Overloaded实例
+
+		//规则二：将亡值。推导为类型的右值引用(???? decltype推倒成右值引用: 或者本来就是 右值引用)
+		decltype (RvalRef()) var5 = 1; // int&& 
+
+		//规则三：左值，推导为类型的引用。
+		decltype ((i))var6 = i;				//int&  带() 返回左值
+		decltype (true ? 0 : 1) var7 = i;	//int&  条件表达式返回左值。
+		decltype (++i) var8 = i;			//int&  ++i返回i的左值。
+		decltype(arr[5]) var9 = i;			//int&. []操作返回左值
+		decltype(*ptr)var10 = i;			//int&  *操作返回左值
+
+		decltype("hello")var11 = "hello";	//const char(&)[6]  字符串字面常量为左值，且为const左值。
+
+		//规则四：以上都不是，则推导为本类型
+		decltype(1) var12;					//const int
+		decltype(Func(1)) var13 = true;		//const bool
+		decltype(i++) var14 = i;			//int	i++返回“右值” 但不是 “将亡值” 
+		decltype(var5) var15 = 2;			//var5 本来就是 右值引用
+	
+		
+
+		cout << "decltype(++i)是否左值引用?" << is_lvalue_reference<decltype(++i)>::value << endl;// 1
+		// decltype(i++) 既不是右值引用 又不是左值引用  就是 int类型
+		cout << "decltype(i++)是否左值引用?" << is_lvalue_reference<decltype(i++)>::value << endl;// 0
+		cout << "decltype(i++)是否右值引用?" << is_rvalue_reference<decltype(i++)>::value << endl;// 0 
+	}
+
     return 0;
 }
 
