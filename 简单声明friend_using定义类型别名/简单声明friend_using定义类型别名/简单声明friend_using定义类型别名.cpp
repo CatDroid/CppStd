352bdb30d8f4cb5422b99cd092a25c281d31982d
hanloong ho <1198432354@qq.com>
2017-12-18 11:22:22 +0800

    简单声明friend C++11可以去掉class 直接friend T  T可以是基本类型或者复杂类型

---
+++ 简单声明friend_using定义类型别名/简单声明friend_using定义类型别名/简单声明friend_using定义类型别名.cpp
@@ -0,0 +1,88 @@
+// 简单声明friend_using定义类型别名.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+template <typename T> class DefenderT {
+public:
+	friend T;  // C++98 不能没有class  C++11支持简单声明friend 
+	void Defencde(int x , int y ){}
+	void Trckle(int x , int y ){}
+
+private:
+	int pos_x = 15;
+	int pos_y = 0;
+	int speed = 2;
+	int stamina = 120;
+};
+
+template <typename T> class AttackerT {
+public:
+	friend T;
+	void Move(int x, int y ){}
+	void SpeedUp(float radio){}
+private:
+	int pos_x = 0;
+	int pos_y = -30;
+	int speed = 3;
+	int stamina = 100;
+
+};
+
+
+/*
+	// 不加 template<>的话  错误 ! 显式特例化前必须有‘template <>’
+	template<>
+	class DefenderT<int> {
+	};
+*/
+
+#define  TEST_UNIT  1 
+
+#ifdef TEST_UNIT 
+ 
+class Validator;
+using DefenderTest = DefenderT<Validator>;
+using AttackerTest = AttackerT<Validator>;
+
+class Validator{
+public:
+	void validate(int x, int y, DefenderTest& d) {}
+	void validate(int x, int y, AttackerTest& a) {}
+};
+
+
+#else
+
+using Attacker = AttackerT<int>;		// 使用using定义类型别名 
+typedef  DefenderT<int>  Defender;		// friend int 不会产生友元
+
+#endif 
+
+
+int main()
+{
+#ifdef TEST_UNIT
+	DefenderTest d;
+	AttackerTest a;
+#else
+	Defender d;
+	Attacker a;
+#endif
+
+	a.Move(10, 30);
+	d.Defencde(15, 30);
+	a.SpeedUp(1.5f);
+	d.Defencde(15, 30);
+
+#ifdef TEST_UNIT
+	Validator v;
+	v.validate(7, 0, d);
+	v.validate(1, -10, a);
+#endif
+
+
+
+    return 0;
+}
+
